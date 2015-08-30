1eb693fad2076f2fba63a10fbdfbbe458309d833
Balaji Arun <balajiarun.dev@hotmail.com>
2015-08-30 10:05:27 -0400

    bochs: add debugger customization option

--- bochs.rb
+++ bochs.rb
@@ -14,34 +14,47 @@ class Bochs < Formula
   depends_on :x11
   depends_on "gtk+"
 
+  option "with-gdb-stub", "Enable GDB Stub"
+
   def install
-    system "./configure", "--prefix=#{prefix}",
-                          "--with-x11",
-                          "--enable-debugger",
-                          "--enable-disasm",
-                          "--disable-docbook",
-                          "--enable-x86-64",
-                          "--enable-pci",
-                          "--enable-all-optimizations",
-                          "--enable-plugins",
-                          "--enable-cdrom",
-                          "--enable-a20-pin",
-                          "--enable-fpu",
-                          "--enable-alignment-check",
-                          "--enable-large-ramfile",
-                          "--enable-debugger-gui",
-                          "--enable-readline",
-                          "--enable-iodebug",
-                          "--enable-xpm",
-                          "--enable-show-ips",
-                          "--enable-logging",
-                          "--enable-usb",
-                          "--enable-ne2000",
-                          "--enable-cpu-level=6",
-                          "--enable-sb16",
-                          "--enable-clgd54xx",
-                          "--with-term",
-                          "--enable-ne2000"
+
+    args = %W[
+      --prefix=#{prefix}
+      --with-x11
+      --with-nogui
+      --enable-disasm
+      --disable-docbook
+      --enable-x86-64
+      --enable-pci
+      --enable-all-optimizations
+      --enable-plugins
+      --enable-cdrom
+      --enable-a20-pin
+      --enable-fpu
+      --enable-alignment-check
+      --enable-large-ramfile
+      --enable-debugger-gui
+      --enable-readline
+      --enable-iodebug
+      --enable-xpm
+      --enable-show-ips
+      --enable-logging
+      --enable-usb
+      --enable-ne2000
+      --enable-cpu-level=6
+      --enable-sb16
+      --enable-clgd54xx
+      --with-term
+      --enable-ne2000
+    ]
+
+    if build.with? "gdb-stub"
+      args << "--enable-gdb-stub"
+    else
+      args << "--enable-debugger"
+    end
+
+    system "./configure", *args
 
     system "make"
     system "make", "install"
