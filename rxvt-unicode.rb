243a212bfd0987f283d887e7804e4fa0558e8106
Andrew Janke <andrew@apjanke.net>
2016-02-23 18:02:18 -0500

    rxvt-unicode: add unicode3 support
    Uses --enable-unicode3 by default. Exposes --without-unicode3 so users
    can get a build that matches the rxvt-unicode distribution's default.
    
    Closes #167
    
    Closes #187.
    
    Signed-off-by: Andrew Janke <andrew@apjanke.net>

--- rxvt-unicode.rb
+++ rxvt-unicode.rb
@@ -1,7 +1,9 @@
 class RxvtUnicode < Formula
+  desc "Rxvt fork with Unicode support"
   homepage "http://software.schmorp.de/pkg/rxvt-unicode.html"
   url "http://dist.schmorp.de/rxvt-unicode/rxvt-unicode-9.22.tar.bz2"
   sha256 "e94628e9bcfa0adb1115d83649f898d6edb4baced44f5d5b769c2eeb8b95addd"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
@@ -11,6 +13,7 @@ class RxvtUnicode < Formula
   end
 
   option "without-iso14755", "Disable ISO 14775 Shift+Ctrl hotkey"
+  option "without-unicode3", "Disable 21-bit Unicode 3 (non-BMP) character support"
 
   deprecated_option "disable-iso14755" => "without-iso14755"
 
@@ -39,6 +42,7 @@ class RxvtUnicode < Formula
 
     args << "--disable-perl" if ENV.compiler == :clang
     args << "--disable-iso14755" if build.without? "iso14755"
+    args << "--enable-unicode3" if build.with? "unicode3"
 
     system "./configure", *args
     system "make", "install"
