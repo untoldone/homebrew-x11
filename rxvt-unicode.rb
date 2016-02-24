41f10b81ca51948bc7d06306f330bbfc8f485245
Andrew Janke <andrew@apjanke.net>
2016-02-23 22:58:54 -0500

    rxvt-unicode: remove glyph-width patch
    This patch was rejected by upstream. Removing it to be consistent
    with upstream distribution's behavior.
    
    Closes #188
    
    Closes #189.
    
    Signed-off-by: Andrew Janke <andrew@apjanke.net>

--- rxvt-unicode.rb
+++ rxvt-unicode.rb
@@ -3,7 +3,7 @@ class RxvtUnicode < Formula
   homepage "http://software.schmorp.de/pkg/rxvt-unicode.html"
   url "http://dist.schmorp.de/rxvt-unicode/rxvt-unicode-9.22.tar.bz2"
   sha256 "e94628e9bcfa0adb1115d83649f898d6edb4baced44f5d5b769c2eeb8b95addd"
-  revision 1
+  revision 2
 
   bottle do
     cellar :any_skip_relocation
@@ -21,11 +21,7 @@ class RxvtUnicode < Formula
   depends_on :x11
 
   # Patches 1 and 2 remove -arch flags for compiling perl support
-  # Patch 3 removes an extra 10% font width that urxvt adds:
-  # https://web.archive.org/web/20111120115603/http://aur.archlinux.org/packages.php?ID=44649
-  # Patch 4 fixes `make install` target on case-insensitive filesystems
-  # (Patch 4 won't be accepted upstream; do not re-submit)
-  # http://lists.schmorp.de/pipermail/rxvt-unicode/2010q2/001186.html
+  # Patch 3 fixes `make install` target on case-insensitive filesystems
   patch :DATA
 
   fails_with :llvm do
@@ -87,32 +83,6 @@ index c756724..5e94907 100755
          PERLPRIVLIBEXP="`$PERL -MConfig -e 'print $Config{privlibexp}'`"
       else
          as_fn_error $? "no, unable to link" "$LINENO" 5
-diff --git a/src/rxvtfont.C b/src/rxvtfont.C
-index 3ff0b04..ecf8196 100644
---- a/src/rxvtfont.C
-+++ b/src/rxvtfont.C
-@@ -1265,12 +1265,21 @@ rxvt_font_xft::load (const rxvt_fontprop &prop, bool force_prop)
-           XGlyphInfo g;
-           XftTextExtents16 (disp, f, &ch, 1, &g);
-
-+/*
-+ * bukind: don't use g.width as a width of a character!
-+ * instead use g.xOff, see e.g.: http://keithp.com/~keithp/render/Xft.tutorial
-+
-           g.width -= g.x;
-
-           int wcw = WCWIDTH (ch);
-           if (wcw > 0) g.width = (g.width + wcw - 1) / wcw;
-
-           if (width    < g.width       ) width    = g.width;
-+ */
-+          int wcw = WCWIDTH (ch);
-+          if (wcw > 1) g.xOff = g.xOff / wcw;
-+          if (width < g.xOff) width = g.xOff;
-+
-           if (height   < g.height      ) height   = g.height;
-           if (glheight < g.height - g.y) glheight = g.height - g.y;
-         }
 diff --git a/Makefile.in b/Makefile.in
 index eee5969..c230930 100644
 --- a/Makefile.in
