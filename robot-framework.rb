64c1ef327e9c5963b0d22a769dec831678739b8e
Lukas Drbal <lukas.drbal@merck.com>
2016-03-04 15:08:40 +0100

    robot-framework: add missing dependencies
    Closes #192.
    
    Signed-off-by: Alex Dunn <adunn@ucsb.edu>

--- robot-framework.rb
+++ robot-framework.rb
@@ -3,6 +3,7 @@ class RobotFramework < Formula
   homepage "http://robotframework.org/"
   url "https://github.com/robotframework/robotframework/archive/3.0.tar.gz"
   sha256 "1b830fa5e4470ff6b2d404bd99b357cfce4a2abd15c255373045a77d44e517a5"
+  revision 1
 
   head "https://github.com/robotframework/robotframework.git", :branch => "master"
   bottle do
@@ -16,13 +17,43 @@ class RobotFramework < Formula
   depends_on :python if MacOS.version <= :snow_leopard
 
   resource "selenium" do
-    url "https://pypi.python.org/packages/source/s/selenium/selenium-2.49.1.tar.gz"
-    sha256 "5fcda19a6d546122bc868a5d36c8e3a40e2593c14b8a3123e5a911b0281fad47"
+    url "https://pypi.python.org/packages/source/s/selenium/selenium-2.51.1.tar.gz"
+    sha256 "05671887f59206abc04d16496c0a3d92ee08497abfc8328a6811eab326fd61a8"
   end
 
   resource "robotframework-selenium2library" do
     url "https://pypi.python.org/packages/source/r/robotframework-selenium2library/robotframework-selenium2library-1.7.4.tar.gz"
     sha256 "4332e3021c6e4ba4a04f4dd99c4fb970315d9fc0fe31bcb62aad2fe833ad9400"
+  end
+
+  resource "decorator" do
+    url "https://pypi.python.org/packages/source/d/decorator/decorator-4.0.6.tar.gz"
+    sha256 "1c6254597777fd003da2e8fb503c3dbf3d9e8f8d55f054709c0e65be3467209c"
+  end
+
+  resource "pycrypto" do
+    url "https://pypi.python.org/packages/source/p/pycrypto/pycrypto-2.6.1.tar.gz"
+    sha256 "f2ce1e989b272cfcb677616763e0a2e7ec659effa67a88aa92b3a65528f60a3c"
+  end
+
+  resource "ecdsa" do
+    url "https://pypi.python.org/packages/source/e/ecdsa/ecdsa-0.13.tar.gz"
+    sha256 "64cf1ee26d1cde3c73c6d7d107f835fed7c6a2904aef9eac223d57ad800c43fa"
+  end
+
+  resource "paramiko" do
+    url "https://pypi.python.org/packages/source/p/paramiko/paramiko-1.16.0.tar.gz"
+    sha256 "3297ebd3cd072f573772f7c7426939a443c62c458d54bb632ff30fd6ecf96892"
+  end
+
+  resource "robotframework-sshlibrary" do
+    url "https://pypi.python.org/packages/source/r/robotframework-sshlibrary/robotframework-sshlibrary-2.1.2.tar.gz"
+    sha256 "aebb57c7b4495f0a3b53e4c111400cdd9d7ebfdfb62af82ca720c727de95803d"
+  end
+
+  resource "robotframework-archivelibrary" do
+    url "https://pypi.python.org/packages/source/r/robotframework-archivelibrary/robotframework-archivelibrary-0.3.2.tar.gz"
+    sha256 "a9b2db827209f53fac77cb5f98b04e655e0933dd607d63da37765e44ebb374f2"
   end
 
   resource "requests" do
@@ -33,6 +64,11 @@ class RobotFramework < Formula
   resource "requests_kerberos" do
     url "https://pypi.python.org/packages/source/r/requests-kerberos/requests-kerberos-0.8.0.tar.gz"
     sha256 "f3ce6e70e19080fe146245f1c663e73a3868fbb087142962b91c5396fa051b1a"
+  end
+
+  resource "kerberos" do
+    url "https://pypi.python.org/packages/source/k/kerberos/kerberos-1.2.4.tar.gz"
+    sha256 "d6f49923bbcf4ebc47ae47b7ceac7866543200669e1dfb24821221da3e940987"
   end
 
   def install
