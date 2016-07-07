0da8d04a5f77927be048da0afc937392ab3b249f
Dominyk Tiller <dominyktiller@gmail.com>
2016-07-07 03:40:00 +0100

    fox 1.6.51
    Closes #218.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- fox.rb
+++ fox.rb
@@ -1,19 +1,14 @@
 class Fox < Formula
+  desc "Toolkit for developing Graphical User Interfaces easily."
   homepage "http://www.fox-toolkit.org/"
-  url "http://ftp.fox-toolkit.org/pub/fox-1.6.50.tar.gz"
-  sha256 "342c751d2a7edab136ad26cb4483329da3c16c7abf2f283f318d666ef12d0887"
+  url "http://ftp.fox-toolkit.org/pub/fox-1.6.51.tar.gz"
+  sha256 "15a99792965d933a4936e48b671c039657546bdec6a318c223ab1131624403d1"
 
   bottle do
     cellar :any
     sha256 "f7e8e7b4ecc4e9cb469b909427adea9ccf72a59c3f89ffd6ea6b006839c6c208" => :yosemite
     sha256 "369b4d8ef5f2dc7e0afae6aa465140c1617d7fb48a14eafd3234974ac3e1ffe0" => :mavericks
     sha256 "08c75fa15ea4146a0fed0e417060a9faf82374884c32d6b0eba226bddf0ece58" => :mountain_lion
-  end
-
-  # Development and stable branches are incompatible
-  devel do
-    url "http://ftp.fox-toolkit.org/pub/fox-1.7.50.tar.gz"
-    sha256 "cdc432807f047b312ab54a778dc898e429dcfecf513fe0d350a7952f523a71df"
   end
 
   depends_on :x11
@@ -34,10 +29,7 @@ class Fox < Formula
   end
 
   test do
-    system bin/"reswrap", "-t",
-           "-o", testpath/"text.txt",
-           test_fixtures("test.jpg")
-    assert_match "\\x00\\x85\\x80\\x0f\\xae\\x03\\xff\\xd9",
-                 (testpath/"text.txt").read
+    system bin/"reswrap", "-t", "-o", "text.txt", test_fixtures("test.jpg")
+    assert_match "\\x00\\x85\\x80\\x0f\\xae\\x03\\xff\\xd9", File.read("text.txt")
   end
 end
