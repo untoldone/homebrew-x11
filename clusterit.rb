e990a8b66f704b9d99949373eb8fa29aa4c3b72c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-07 04:00:24 +0100

    clusterit: add 2.5 bottle.

--- clusterit.rb
+++ clusterit.rb
@@ -4,6 +4,13 @@ class Clusterit < Formula
   url "https://downloads.sourceforge.net/project/clusterit/clusterit/clusterit-2.5/clusterit-2.5.tar.gz"
   sha256 "e50597fb361d9aefff0250108900a3837a4a14c46083d6eb5ed5d7fc42ce9f35"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "566ccf9d65e0cc4a3a04d4c6d15ebbf4829b92688142c9f1b9f38fd835f7c305" => :el_capitan
+    sha256 "374a73eab22211e6df8460c1826a31afe4522861bf3d7bfeb13ec7a3fdb1f133" => :yosemite
+    sha256 "4e5e4239e7dc5e9e0f52dbc84b4190693132b5b327acd7e3beba11e46208c831" => :mavericks
+  end
+
   depends_on :x11
 
   conflicts_with "couchdb-lucene", :because => "both install a `run` binary"
