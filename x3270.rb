9b4021a6651be4c8e98d9227b36fd5a0c82a110c
Miguel Araújo <miguelarauj1o@gmail.com>
2016-08-06 12:55:35 -0300

    x3270: add description and code position refactoring
    Closes #226.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- x3270.rb
+++ x3270.rb
@@ -1,4 +1,5 @@
 class X3270 < Formula
+  desc "IBM 3270 terminal emulator for the X Window System and Windows"
   homepage "http://x3270.bgp.nu/"
   url "https://downloads.sourceforge.net/project/x3270/x3270/3.4ga8/suite3270-3.4ga8-src.tgz"
   sha256 "06be4d79ffc24f4465b167b08c6ec48b595f689f3c5177ce5902fb31560b5dfd"
@@ -9,13 +10,13 @@ class X3270 < Formula
     sha256 "8d3305ecc979f02af1795310f979358b695ac9462b6aa1aa2f9fb7aedb574992" => :mavericks
   end
 
-  depends_on :x11
-  depends_on "openssl"
-
   option "with-c3270", "Include c3270 (curses-based version)"
   option "with-s3270", "Include s3270 (displayless version)"
   option "with-tcl3270", "Include tcl3270 (integrated with Tcl)"
   option "with-pr3287", "Include pr3287 (printer emulation)"
+
+  depends_on :x11
+  depends_on "openssl"
 
   def install
     args = ["--prefix=#{prefix}"]
