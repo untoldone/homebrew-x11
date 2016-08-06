bb1ce556dc0903b1d4d10511ea99d4674162d248
Miguel Araújo <miguelarauj1o@gmail.com>
2016-08-06 12:44:24 -0300

    xclip: add desc and update url
    Closes #225.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- xclip.rb
+++ xclip.rb
@@ -1,5 +1,6 @@
 class Xclip < Formula
-  homepage "http://xclip.sourceforge.net/"
+  desc "Command-line utility that is designed to run on any system with an X11"
+  homepage "https://github.com/astrand/xclip"
   url "https://downloads.sourceforge.net/project/xclip/xclip/0.12/xclip-0.12.tar.gz"
   sha256 "b7c7fad059ba446df5692d175c2a1d3816e542549661224806db369a0d716c45"
 
