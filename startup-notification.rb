ce7a93fb9d69309d8fbe3d10c116897c297c5e4d
Miguel Araújo <miguelarauj1o@gmail.com>
2016-08-06 22:46:41 -0300

    startup-notification: add description
    Closes #231.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- startup-notification.rb
+++ startup-notification.rb
@@ -1,6 +1,7 @@
 class StartupNotification < Formula
-  homepage "http://www.freedesktop.org/wiki/Software/startup-notification/"
-  url "http://www.freedesktop.org/software/startup-notification/releases/startup-notification-0.12.tar.gz"
+  desc "Reference implementation of the startup notification protocol"
+  homepage "https://www.freedesktop.org/wiki/Software/startup-notification/"
+  url "https://www.freedesktop.org/software/startup-notification/releases/startup-notification-0.12.tar.gz"
   sha256 "3c391f7e930c583095045cd2d10eb73a64f085c7fde9d260f2652c7cb3cfbe4a"
 
   depends_on "pkg-config" => :build
